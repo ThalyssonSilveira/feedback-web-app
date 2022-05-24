52d2a4ded3ba755bbee4f371b6edd1fafa5c4356
Thalysson <thalysson7@gmail.com>
2022-05-24 10:04:03 -0300

    Create Average Rating and Input Form

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,15 @@
-function FeedbackItem({item}) {
-    
+import { FaTimes } from 'react-icons/fa'
+import Card from './shared/Card'
+
+function FeedbackItem({ item, handleDelete }) {
     return (
-        <div className="card">
+        <Card>
             <div className="num-display">{item.rating}</div>
+            <button className="close">
+                <FaTimes onClick={() => handleDelete(item.id)} color="purple" />
+            </button>
             <div className="text-display">{item.text}</div>
-        </div>
+        </Card>
     )
 }
 
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,16 +1,19 @@
-import FeedbackItem from "./FeedbackItem"
+import FeedbackItem from './FeedbackItem'
 
-function FeedbackList({feedback}) {
-  
-    if(!feedback || feedback.length === 0) return <p>No Feeddback Yet</p>   
-  
+function FeedbackList({ feedback, handleDelete }) {
+    if (!feedback || feedback.length === 0) return <p>No Feeddback Yet</p>
+
     return (
-    <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item}/>
-      ))}
-    </div>
-  )
+        <div className="feedback-list">
+            {feedback.map((item) => (
+                <FeedbackItem
+                    key={item.id}
+                    item={item}
+                    handleDelete={handleDelete}
+                />
+            ))}
+        </div>
+    )
 }
 
 export default FeedbackList
