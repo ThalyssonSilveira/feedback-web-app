bdbfdc980e9392677a27c2ea8ea30d01f3c750d1
Thalysson <thalysson7@gmail.com>
2022-05-25 13:30:04 -0300

    Create Context

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,7 +1,11 @@
 import { motion, AnimatePresence } from 'framer-motion'
+import { useContext } from 'react'
 import FeedbackItem from './FeedbackItem'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList() {
+    const { feedback } = useContext(FeedbackContext)
+
     if (!feedback || feedback.length === 0) return <p>No Feeddback Yet</p>
 
     return (
@@ -14,11 +18,7 @@ function FeedbackList({ feedback, handleDelete }) {
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
                     >
-                        <FeedbackItem
-                            key={item.id}
-                            item={item}
-                            handleDelete={handleDelete}
-                        />
+                        <FeedbackItem key={item.id} item={item} />
                     </motion.div>
                 ))}
             </AnimatePresence>
